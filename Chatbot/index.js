9e54f6b3fc410062543ef1d6ecc3659163a1ddcd
Hiruni Ranasinghe <ranasinghehiruni14@gmail.com>
2024-11-13 12:59:43 +0530

    creating

--- Chatbot/index.js
+++ Chatbot/index.js
@@ -3,8 +3,11 @@ var ejs = require('ejs')
 
 var app = express();
 
+app.use(express.static('public'))
+app.set('view engine','ejs');
+
 app.listen(8080);
 
 app.get('/',function(req,res){
-    res.send("Hello");
+    res.render('pages/index');
 });
