b52cc3e7cea4df6606efebb8a1ab027737c47587
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-11-29 07:50:20 +0000

    db connect

--- ShareMyRide/index.js
+++ ShareMyRide/index.js
@@ -5,7 +5,7 @@ const mongoose=require("mongoose");
 const authRouter=require("./routes/authRoute")
 const rideRoute=require("./routes/rideRoute")
 
-mongoose.connect("mongodb+srv://adithyagunasekara2000:<YvovidOF8PJwJoTI>@cluster0.ksfu6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb+srv://thathsaraniadithyaa:2009Adith@socialconnect.ykuwxon.mongodb.net/?retryWrites=true&w=majority&appName=socialConnect");
 var db = mongoose.connection;
 db.on('error', () => console.log("Error in connecting to DB"));
 db.once('open', () => console.log("Connected to DB"));
@@ -15,7 +15,7 @@ app.use("/api/auth",authRouter);
 app.use("/api/ride",rideRoute);
 
 
-app.listen(2001,()=>{
+app.listen(2002,()=>{
     console.log("Backend server is running")
 });
 // YvovidOF8PJwJoTI
