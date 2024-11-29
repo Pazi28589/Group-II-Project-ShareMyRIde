7fc3774b0fa0bd075b0c92c851d1d09b5351f3e4
Adithyagunasekara2000 <139615785+Adithyagunasekara2000@users.noreply.github.com>
2024-11-29 06:12:41 +0000

    try

--- ShareMyRide/index.js
+++ ShareMyRide/index.js
@@ -3,10 +3,10 @@ const app=new express();
 const mongoose=require("mongoose");
 const authRouter=require("./routes/authRoute")
 
-mongoose.connect("mongodb://localhost:27017/shareMyRide");
+/*mongoose.connect("mongodb://localhost:27017/shareMyRide");
 var db = mongoose.connection;
 db.on('error', () => console.log("Error in connecting to DB"));
-db.once('open', () => console.log("Connected to DB"));
+db.once('open', () => console.log("Connected to DB"));*/
 
 app.use(express.json());
 app.use("/api/auth",authRouter);
