2eb4340758434ca93d681bcd48e003d6b0e9e1c1
kaveeshabasnayaka <139634637+kaveeshabasnayaka@users.noreply.github.com>
2024-11-29 06:58:55 +0000

    check rideRoute running

--- ShareMyRide/index.js
+++ ShareMyRide/index.js
@@ -1,7 +1,9 @@
 const express=require("express");
 const app=new express();
 const mongoose=require("mongoose");
+
 const authRouter=require("./routes/authRoute")
+const rideRoute=require("./routes/rideRoute")
 
 mongoose.connect("mongodb://localhost:27017/shareMyRide");
 var db = mongoose.connection;
@@ -10,6 +12,8 @@ db.once('open', () => console.log("Connected to DB"));
 app.use(express.json());
 
 app.use("/api/auth",authRouter);
+app.use("/api/ride",rideRoute);
+
 
 app.listen(2001,()=>{
     console.log("Backend server is running")
