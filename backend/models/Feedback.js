d8577040c7ab8d08cecc91af0d6b30c9d0f5fa0c
prathibhani <139643234+prathibhani@users.noreply.github.com>
2024-11-30 16:04:58 +0000

    created the models of feedback.js

--- backend/models/Feedback.js
+++ backend/models/Feedback.js
@@ -4,6 +4,11 @@ const Schema =mongoose.Schema;
 const feedbackSchema=new Schema(
     {
 userId:{type:Schema.Types.ObjectId,ref: 'User',required:true},
-rideId:{}
+rideId:{type:Schema.Types.ObjectId,ref:'Ride',required:true},
+rating:{type:Number,min:1,max:5,required:true},
+comment:{type:String,required:false},
+feedbackType:{type:String,enum:['driver','passenger'],required:true},
+createdAt:{type:Date,default:Date.now}
     
     });
+    module.exports=mongoose.model('Feedback',feedbackSchema);
