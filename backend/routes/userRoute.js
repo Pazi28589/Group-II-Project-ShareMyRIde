439d1ae36051e9f014255746fcecc4a530553385
prathibhani <139643234+prathibhani@users.noreply.github.com>
2024-12-01 17:29:37 +0000

    User Registered in userRoute

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -7,3 +7,29 @@ const jwt=require('jsonwebtoken');
 const auth=require('../Security/auth');
 const secretKey='project@shareMyRide';
 
+// Register User
+router.post("/register",async(req,res)=>{
+try{
+    if(req.body.password !== req.body.confirmPassword){
+        return res.status(400).json({message:"Passwords do not match"});
+
+    }
+    const salt=await bcrypt.genSalt(10);
+    const hashedPassword=await bcrypt.hash(req.body.password,salt);
+
+    const newUser=new User({
+        firstname:req.body.firstname,
+        lastname:req.body.lastname,
+        email:req.body.email,
+        password:hashedPassword,
+        mobileNumber:req.body.mobileNumber,
+        NIC:req.body.NIC
+    });
+    const user=await newUser.save();
+    res.status(200).json(user);
+}
+catch(err){
+    res.status(500).json({error:err.message});
+
+}
+});
