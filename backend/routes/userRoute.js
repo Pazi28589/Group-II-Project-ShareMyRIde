2d2b5d3cebd17eefcab4f60272d4e620273ad64b
prathibhani <139643234+prathibhani@users.noreply.github.com>
2024-12-06 00:58:53 +0000

    User Logged in userRoute

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -33,3 +33,24 @@ catch(err){
 
 }
 });
+
+router.post('/login',async(req,res)=>{
+    const {NIC,password}=req.body;
+    try{
+        const user=await User.findOne({NIC});
+        if(!user)return res.status(404).send({message:'User not found'});
+
+        const isValidPassword =await bcrypt.compare(password,user.password);
+        if(!isValidPassword) return res.status(400).send({message:'Invalid password'});
+
+        const token =jwt.sign({userId:user._id},secretKey,{expiresIn: '10h'});
+        console.log('Generated Token:',token);
+
+        res.send({token,user:{id:user._id,username:user.firstname,email:user.email}});
+
+    }
+    catch(err){
+        console.error("Error during login:",err);
+        res.status(500).json({error:err.message});
+    }
+});
