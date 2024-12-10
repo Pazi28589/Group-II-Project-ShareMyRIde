adfeed9190925e3e6d51209aaeb97ac92565ce45
prathibhani <139643234+prathibhani@users.noreply.github.com>
2024-12-10 02:05:54 +0000

    deleted loggin user in userRoute

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -9,27 +9,8 @@ const secretKey='project@shareMyRide';
 
 
 
-//login user
-router.post('/login',async(req,res)=>{
-    const {NIC,password}=req.body;
-    try{
-        const user=await User.findOne({NIC});
-        if(!user)return res.status(404).send({message:'User not found'});
 
-        const isValidPassword =await bcrypt.compare(password,user.password);
-        if(!isValidPassword) return res.status(400).send({message:'Invalid password'});
 
-        const token =jwt.sign({userId:user._id},secretKey,{expiresIn: '10h'});
-        console.log('Generated Token:',token);
-
-        res.send({token,user:{id:user._id,username:user.firstname,email:user.email}});
-
-    }
-    catch(err){
-        console.error("Error during login:",err);
-        res.status(500).json({error:err.message});
-    }
-});
 //submit feedback
 router.post('/feedback',auth,async(req,res)=>{
     const{rideId,rating,comment,feedbackType}=req.body;
