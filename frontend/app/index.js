72651ee121195e0111501bfedb9d7577a20b25e6
W A D G H Athukorala <gimanthaheshan2000@gmail.com>
2024-12-13 16:35:11 +0530

    create register page

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Pressable, Text, View } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function App() {
-  return (
-    <View className="h-screen w-screen flex items-center justify-center">
-      <Text>Share My Ride</Text>
-      <Text className="text-red-500">Welcome</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+      <View className="h-screen w-screen flex items-center justify-center">
+        <Text>Share My Ride</Text>
+        <Text className="text-red-500">Welcome</Text>
+        <StatusBar style="auto" />
+
+        <Link href="/register" asChild>
+          <Pressable className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            <Text>Next</Text>
+          </Pressable>
+        </Link>
+      </View>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
