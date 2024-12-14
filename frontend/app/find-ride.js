2a3093ba8bf9515e8c6a3ff73c0b7ce21e0555be
Rivindu_Pabasara <101160321+Rivindu10@users.noreply.github.com>
2024-12-14 22:12:10 +0530

    create find-ride

---
+++ frontend/app/find-ride.js
@@ -0,0 +1,16 @@
+import React from 'react'
+//import MapView from 'react-native-maps';
+import { StyleSheet, Text, View } from 'react-native';
+
+const FindRide = () => {
+  return (
+    <View>
+    <Text>
+        Find your ride from Here
+    </Text>
+    
+  </View>
+  )
+}
+
+export default FindRide;
