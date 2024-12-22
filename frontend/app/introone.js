4b9ae48b3f4972941c12f0c7ff21103bad552e13
Rivindu_Pabasara <101160321+Rivindu10@users.noreply.github.com>
2024-12-22 21:10:11 +0530

    fixing errors in introone.js file

--- frontend/app/introone.js
+++ frontend/app/introone.js
@@ -13,9 +13,10 @@ const introone = () => {
       </Text>
       <View className="flex items-center justify-center flex-row gap-10">
 
-        <Pressable onPress={()=>{router.push("/login")}} 
-        className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-          <Text className="text-white">Skip</Text>
+        
+        <Pressable  onPress={() => { router.push("/login") }} 
+         className="bg-gray-200 mt-4 hover:bg-gray-300 text-black font-bold py-2 px-4 rounded">
+            <Text className="text-black">Skip</Text>
         </Pressable>
 
         <Pressable  onPress={()=>{router.push("/login")}} 
