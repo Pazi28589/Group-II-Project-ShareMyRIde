375783b923bf940db365bddb51802520e18fd16c
Pasindu Ravishka Hettiarachchi <154882714+Pazi28589@users.noreply.github.com>
2024-12-25 14:38:53 +0530

    Update add-ride.js

--- frontend/app/add-ride.js
+++ frontend/app/add-ride.js
@@ -8,7 +8,13 @@ const AddRide = () => {
     <Text>
         Add Your Ride Here
     </Text>
-    
+    <div className="flex-center full-screen">
+      <div className="text-center">
+        <h1 className="header">Add a New Ride</h1>
+        <input className="input-field" type="text" placeholder="Enter Ride Details" />
+        <button className="button-primary">Submit</button>
+      </div>
+    </div>
   </View>
   )
 }
